cd201e3b246ec6232d79143d7c5caea309c14de6
twhitloc <tyler.whitlock@gmail.com>
2015-09-02 13:28:37 -0400

    Fixed bracket issue

--- scripts/Main.js
+++ scripts/Main.js
@@ -13,8 +13,7 @@ define(['http://worldwindserver.net/webworldwind/worldwindlib.js',
     'http://worldwindserver.net/webworldwind/examples/LayerManager.js',
     'UGSDataRetriever',
     'EarthquakeLayer',
-    'barGraphRepresentation'],function(ww,LayerManager,EarthQuakeRetrieval,createPlaceMarks,barGraphRepresentation)
-{
+    'barGraphRepresentation'],function(ww,LayerManager,EarthQuakeRetrieval,createPlaceMarks,barGraphRepresentation) {
     'use strict';
 
     // instantiate the sliders
@@ -25,9 +24,9 @@ define(['http://worldwindserver.net/webworldwind/worldwindlib.js',
     });
 
     var ageSlider = new Slider('#ageSlider', {
-        formatter: function (value){
-            if(value != 80)
-                return  value + ' hours';
+        formatter: function (value) {
+            if (value != 80)
+                return value + ' hours';
             else
                 return "Show All Available Data"
         }
@@ -40,12 +39,8 @@ define(['http://worldwindserver.net/webworldwind/worldwindlib.js',
 
     var cb = new Codebird;
 
-    cb.setConsumerKey("VddGNUN9GWxbbKBoHDzhNRjjo","noC4s8BEKQCu4gZoXx2E13CYWzbA7gUjL9dM35IwJLtErfKTjb");
-    cb.setToken("3416857132-Fv8A8BIrbb7OGYoUODrfDb8bDvqhu1OBbusFzgj","24qSgQIOfVBWiSudRI1GX9EivqrneqOqlG3c42gdA20Ny");
-
-
-
-
+    cb.setConsumerKey("VddGNUN9GWxbbKBoHDzhNRjjo", "noC4s8BEKQCu4gZoXx2E13CYWzbA7gUjL9dM35IwJLtErfKTjb");
+    cb.setToken("3416857132-Fv8A8BIrbb7OGYoUODrfDb8bDvqhu1OBbusFzgj", "24qSgQIOfVBWiSudRI1GX9EivqrneqOqlG3c42gdA20Ny");
 
 
 // Define the event listener to initialize Web World Wind.
@@ -73,13 +68,13 @@ define(['http://worldwindserver.net/webworldwind/worldwindlib.js',
         }
 
         // Create the Layer Manager to enable user to select certain layers.
-        var layerManager= new LayerManager(wwd);
+        var layerManager = new LayerManager(wwd);
 
 
-        var earthQuakes =  new EarthQuakeRetrieval();
+        var earthQuakes = new EarthQuakeRetrieval();
         var placeMark = new createPlaceMarks(wwd, earthQuakes);
         var barGraph = new barGraphRepresentation(wwd, earthQuakes);
-        var previousStatus ="";
+        var previousStatus = "";
 
         ///
         ///
@@ -89,23 +84,20 @@ define(['http://worldwindserver.net/webworldwind/worldwindlib.js',
         makeTwitterCalls(cb, earthQuakes, previousStatus, placeMark);
 
 
-
         ///Timer function----------------
         //
         // window.setInterval(updateTimerEvent, 3000);
-        setInterval(function() {
+        setInterval(function () {
             updateTimerEvent(earthQuakes, placeMark, (cb = new Codebird()), previousStatus)
-        }, 300000 );
-
-
+        }, 300000);
 
 
         //Function to perform on slider stop
-        magnitudeSlider.on('slideStop', function(value){
+        magnitudeSlider.on('slideStop', function (value) {
             //value is the new value for this slider
             slideFunction();
         });
-        ageSlider.on('slideStop', function(value){
+        ageSlider.on('slideStop', function (value) {
             //value is the new value for this slider
             slideFunction();
         });
@@ -129,7 +121,7 @@ define(['http://worldwindserver.net/webworldwind/worldwindlib.js',
         // If maxAge=80 represent user wanting to display all available data
         // so we set the maximum age to a month because thats pretty much all the data we will ever have
         //  from our current USGS server at any given moment
-        if(maxAge ==80) maxAge = 24*31;
+        if (maxAge == 80) maxAge = 24 * 31;
 
         console.log(minMag);
 
@@ -140,14 +132,14 @@ define(['http://worldwindserver.net/webworldwind/worldwindlib.js',
                 layerPlacemarks = wwd.layers[i];
 
             }
-            else if(wwd.layers[i].displayName == "3D EarthQuake View") {
+            else if (wwd.layers[i].displayName == "3D EarthQuake View") {
                 layer3DGraphData = wwd.layers[i];
             }
         }
 
-        for(var i = 0; i <layerPlacemarks.renderables.length; i++) {
+        for (var i = 0; i < layerPlacemarks.renderables.length; i++) {
 
-            var magnitudeCondition =  layerPlacemarks.renderables[i].magnitude >= minMag;
+            var magnitudeCondition = layerPlacemarks.renderables[i].magnitude >= minMag;
             var ageCondition = layerPlacemarks.renderables[i].ageInHours <= maxAge;
 
             // A certain renderable is enabled if its magnitude value is greater or equal to the slider value
@@ -156,9 +148,9 @@ define(['http://worldwindserver.net/webworldwind/worldwindlib.js',
 
         }
 
-        for(var i = 0; i <layer3DGraphData.renderables.length; i++) {
+        for (var i = 0; i < layer3DGraphData.renderables.length; i++) {
 
-            var magnitudeCondition =  layer3DGraphData.renderables[i].magnitude >= minMag;
+            var magnitudeCondition = layer3DGraphData.renderables[i].magnitude >= minMag;
             var ageCondition = layer3DGraphData.renderables[i].ageInHours <= maxAge;
 
             // A certain renderable is enabled if its magnitude value is greater or equal to the slider value
@@ -169,7 +161,6 @@ define(['http://worldwindserver.net/webworldwind/worldwindlib.js',
         }
 
     };
-
 
 
     /*   -----------------------------------------------------------------------
@@ -220,87 +211,79 @@ define(['http://worldwindserver.net/webworldwind/worldwindlib.js',
     document.getElementById("layer2D").onclick = layer2DSelected;
 
 
+    /*   -----------------------------------------------------------------------
+     @Description: This is the program that controls the refreshing rate
+     of the earthquake events that have been logged by USGS.  They are set for every 5 minutes.
+     This will tweet all new earthquakes in the last update in the order they were reported.
 
-});
+     @Param: NONE
+     @return:  NONE
 
+     --------------------------------------------------------------------------------*/
+    function updateTimerEvent(earthQuakes, placeMark, cb, previousStatus) { //every 5 minutes check earthquakes
+        'use strict';
 
+        var updatedEarthQuakes = new EarthQuakeRetrieval();
 
+        var indexofpreviousRecent = -1;
+        var i = 0;
 
-/*   -----------------------------------------------------------------------
- @Description: This is the program that controls the refreshing rate
- of the earthquake events that have been logged by USGS.  They are set for every 5 minutes.
- This will tweet all new earthquakes in the last update in the order they were reported.
-
- @Param: NONE
- @return:  NONE
-
- --------------------------------------------------------------------------------*/
-function updateTimerEvent(earthQuakes, placeMark, cb, previousStatus) { //every 5 minutes check earthquakes
-    'use strict';
-
-    var updatedEarthQuakes = new EarthQuakeRetrieval();
-
-    var indexofpreviousRecent =-1;
-    var i =0;
-
-    while(indexofpreviousRecent == -1) {
-        if ((earthQuakes[0].title != updatedEarthQuakes[i].title) && (earthQuakes[0].depth != updatedEarthQuakes[i].depth) && (earthQuakes[0].magnitude != updatedEarthQuakes[i].magnitude)) {
-            i++
-        }
-        else
-        {
-            indexofpreviousRecent = i;
-        }
-
-    }
-
-    earthQuakes == updatedEarthQuakes;
-    if (indexofpreviousRecent !=0)
-    { //if the earthquakes are different check the latest tweet
-
-        cb.setConsumerKey("VddGNUN9GWxbbKBoHDzhNRjjo", "noC4s8BEKQCu4gZoXx2E13CYWzbA7gUjL9dM35IwJLtErfKTjb");
-        cb.setToken("3416857132-Fv8A8BIrbb7OGYoUODrfDb8bDvqhu1OBbusFzgj", "24qSgQIOfVBWiSudRI1GX9EivqrneqOqlG3c42gdA20Ny");
-
-        var params = {
-            status: placeMark.getTweetText(earthQuakes[0])
-        };
-
-        cb.__call(
-            "account_verifyCredentials",
-            {},
-            function (reply) {  //get the account and see what last tweet was
-                previousStatus = reply.status.text;
-
-                earthQuakes = updatedEarthQuakes;
-                for(var i = indexofpreviousRecent-1; i >= 0; i--) //for each new earthquake check to see if it was the last tweet
-                {
-                    if ((previousStatus.indexOf(updatedEarthQuakes[i].title)<0) && (previousStatus.indexOf("Depth : " + updatedEarthQuakes[i].depth + "kilometers")<0) && (previousStatus.indexOf(" M " + updatedEarthQuakes[i].magnitude.toString())<0))
-                    {
-                        var params = {
-                            status: placeMark.getTweetText(updatedEarthQuakes[i])
-                        };
-
-                        cb.__call(
-                            "statuses_update",
-                            params,
-                            function (reply) {
-                                //Currently we don't need to do anything with the reply received from twitter in JSON format.
-                                console.log(reply);
-                            }
-                        );
-
-                    }
-                }
-
-                placeMark = new createPlaceMarks(wwd, earthQuakes);
-                wwd.redraw();
-                console.log(reply); //reply from getting the user information
+        while (indexofpreviousRecent == -1) {
+            if ((earthQuakes[0].title != updatedEarthQuakes[i].title) && (earthQuakes[0].depth != updatedEarthQuakes[i].depth) && (earthQuakes[0].magnitude != updatedEarthQuakes[i].magnitude)) {
+                i++
             }
-        );
+            else {
+                indexofpreviousRecent = i;
+            }
+
+        }
+
+        earthQuakes == updatedEarthQuakes;
+        if (indexofpreviousRecent != 0) { //if the earthquakes are different check the latest tweet
+
+            cb.setConsumerKey("VddGNUN9GWxbbKBoHDzhNRjjo", "noC4s8BEKQCu4gZoXx2E13CYWzbA7gUjL9dM35IwJLtErfKTjb");
+            cb.setToken("3416857132-Fv8A8BIrbb7OGYoUODrfDb8bDvqhu1OBbusFzgj", "24qSgQIOfVBWiSudRI1GX9EivqrneqOqlG3c42gdA20Ny");
+
+            var params = {
+                status: placeMark.getTweetText(earthQuakes[0])
+            };
+
+            cb.__call(
+                "account_verifyCredentials",
+                {},
+                function (reply) {  //get the account and see what last tweet was
+                    previousStatus = reply.status.text;
+
+                    earthQuakes = updatedEarthQuakes;
+                    for (var i = indexofpreviousRecent - 1; i >= 0; i--) //for each new earthquake check to see if it was the last tweet
+                    {
+                        if ((previousStatus.indexOf(updatedEarthQuakes[i].title) < 0) && (previousStatus.indexOf("Depth : " + updatedEarthQuakes[i].depth + "kilometers") < 0) && (previousStatus.indexOf(" M " + updatedEarthQuakes[i].magnitude.toString()) < 0)) {
+                            var params = {
+                                status: placeMark.getTweetText(updatedEarthQuakes[i])
+                            };
+
+                            cb.__call(
+                                "statuses_update",
+                                params,
+                                function (reply) {
+                                    //Currently we don't need to do anything with the reply received from twitter in JSON format.
+                                    console.log(reply);
+                                }
+                            );
+
+                        }
+                    }
+
+                    placeMark = new createPlaceMarks(wwd, earthQuakes);
+                    wwd.redraw();
+                    console.log(reply); //reply from getting the user information
+                }
+            );
+
+        }
 
     }
 
-}
 //end of timer-----------
 
 ///Description -
@@ -308,33 +291,32 @@ function updateTimerEvent(earthQuakes, placeMark, cb, previousStatus) { //every 
 /// if the current most-recent is not the same as the last tweet,
 /// tweet the earthquake obtained from the initialization of the session
 ///
-function makeTwitterCalls(cb, earthQuakes, previousStatus, placeMark)
-{
-    cb.__call(
-        "account_verifyCredentials",
-        {},
-        function (reply) {  //get the account and see what last tweet was
-            previousStatus = reply.status.text;
-            var sx = previousStatus.toString().indexOf("&lt;!&gt;");
+    function makeTwitterCalls(cb, earthQuakes, previousStatus, placeMark) {
+        cb.__call(
+            "account_verifyCredentials",
+            {},
+            function (reply) {  //get the account and see what last tweet was
+                previousStatus = reply.status.text;
+                var sx = previousStatus.toString().indexOf("&lt;!&gt;");
 
-            if ((previousStatus.toString().indexOf(earthQuakes[0].title)<0) && (previousStatus.toString().indexOf("Depth : " + earthQuakes[0].depth + "kilometers")<0) && (previousStatus.toString().indexOf(" M " + earthQuakes[0].magnitude.toString())<0))
-            {
-                sx = previousStatus.toString().indexOf(earthQuakes[0].title);
-                sx = previousStatus.toString().indexOf("Depth : " + earthQuakes[0].depth + "kilometers");
-                var params = {
-                    status: placeMark.getTweetText(earthQuakes[0])
-                };
+                if ((previousStatus.toString().indexOf(earthQuakes[0].title) < 0) && (previousStatus.toString().indexOf("Depth : " + earthQuakes[0].depth + "kilometers") < 0) && (previousStatus.toString().indexOf(" M " + earthQuakes[0].magnitude.toString()) < 0)) {
+                    sx = previousStatus.toString().indexOf(earthQuakes[0].title);
+                    sx = previousStatus.toString().indexOf("Depth : " + earthQuakes[0].depth + "kilometers");
+                    var params = {
+                        status: placeMark.getTweetText(earthQuakes[0])
+                    };
 
 
-                cb.__call(
-                    "statuses_update",
-                    params,
-                    function (reply) {
-                        //Currently we don't need to do anything with the reply received from twitter in JSON format.
-                        console.log(reply);
-                    }
-                );
+                    cb.__call(
+                        "statuses_update",
+                        params,
+                        function (reply) {
+                            //Currently we don't need to do anything with the reply received from twitter in JSON format.
+                            console.log(reply);
+                        }
+                    );
+                }
             }
-        }
-    );
-}
+        );
+    }
+});
